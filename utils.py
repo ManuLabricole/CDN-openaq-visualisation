c7a0b11b90341755d07e20d2abadaed23c903ade
Manu Labricole <manu_barriol@hotmail.fr>
2023-06-22 13:46:04 +0200

    build function for datetime and coordinate

--- utils.py
+++ utils.py
@@ -1,5 +1,22 @@
+import ast
+
 import pandas as pd
 
 
 def save_df_to_csv(df:pd.DataFrame, name:str) -> None:
     df.to_csv(name, index=False)
+    
+    
+def convert_to_datetime(df:pd.DataFrame, column:str = "date") -> pd.DataFrame:
+    
+    df[column] = df[column].map(lambda x: x['utc'])
+    df[column] = pd.to_datetime(df[column])
+    
+    return df    
+
+def generate_coordinate_columns(df):    
+        
+    df['latitude'] = df['coordinates'].map(lambda x: x['latitude'])
+    df['longitude'] = df['coordinates'].map(lambda x: x['longitude'])
+    
+        
